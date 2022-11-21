6f672e23b90532f95b89a7490bf40c29f65251c9
okonma-violet <okonmaviolet@gmail.com>
2022-11-21 21:29:04 +0500

    tst

--- main.go
+++ main.go
@@ -2,24 +2,106 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
+type repo struct {
+	brandsById   map[int]*brand
+	brandsByNorm map[string]*brand
+	//categiriesByName   map[string]*category
+	categiriesById    map[int]*category
+	productsByArticul map[string]*product
+
+	ngkArticules map[string][]string
+}
+
+type brand struct {
+	id   int
+	name string
+	norm []string
+}
+
+type category struct {
+	id      int
+	name    string
+	phrases []string
+}
+
+type product struct {
+	id          int
+	brand_id    int
+	category_id int
+	name        string
+	articul     string
+}
+
 func main() {
 	println("CONVERT")
-	convert("prices/")
+	convert("test/prices/")
 	println("CONVERT DONE")
 
 	println("MOVE CSV")
-	movefiles(".", "prices/csv/", ".csv")
+	movefiles(".", "test/prices/csv/", ".csv")
 	println("MOVE CSV DONE")
 
-	//println("LOAD BRANDS")
+	r := &repo{}
 
-	//println("LOAD BRANDS DONE")
+	println("LOAD BRANDS")
+	r.loadbrands("test/refs/brands.csv")
+	println("LOAD BRANDS DONE")
+
+	println("LOAD CATEGORIES")
+	r.loadcategories("test/refs/categories.csv")
+	println("LOAD CATEGORIES DONE")
+
+	println("LOAD NGK ARTS")
+	r.loadNGKarticules("test/refs/ngk.txt")
+	println("LOAD NGK ARTS DONE")
+
+	println("LOAD PRODUCTS")
+	r.loadproducts("test/prices/csv/")
+	println("LOAD PRODUCTS DONE")
+
+	println("CATEGORIZING")
+	r.categorize()
+	println("CATEGORIZING DONE")
+
+	fmt.Println("\n-----------------\n")
+	var i int
+	fmt.Println("\nbrands by id")
+	for j, k := range r.brandsById {
+		if i < 10 {
+			fmt.Println("++ id", j, k)
+			i++
+		} else {
+			break
+		}
+	}
+	fmt.Println("\nbrands by norm")
+	for j, k := range r.brandsByNorm {
+		if i > 0 {
+			fmt.Println("-- norm", j, k)
+			i--
+		} else {
+			break
+		}
+	}
+	fmt.Println("\ncategories by id")
+	for j, k := range r.categiriesById {
+		fmt.Println("$$ id", j, k)
+	}
+	fmt.Println("\nproducts by art")
+	for j, k := range r.productsByArticul {
+		fmt.Println("## art", j, k)
+	}
+}
+
+func uploadProductsToCSV() {
+
 }
 
 func convert(path string) {
@@ -50,36 +132,6 @@ func movefiles(currpath string, newpath string, files_ext string) {
 			}
 		}
 	}
-}
-
-type repo struct {
-	brandsById   map[int]*brand
-	brandsByNorm map[string]*brand
-	//categiriesByName   map[string]*category
-	categiriesById    map[int]*category
-	productsByArticul map[string]*product
-
-	ngkArticules map[string][]string
-}
-
-type brand struct {
-	id   int
-	name string
-	norm []string
-}
-
-type category struct {
-	id      int
-	name    string
-	phrases []string
-}
-
-type product struct {
-	id          int
-	brand_id    int
-	category_id int
-	name        string
-	articul     string
 }
 
 func (r *repo) loadbrands(filepath string) {
@@ -180,7 +232,9 @@ func (r *repo) loadproducts(path string) {
 	}
 	r.productsByArticul = make(map[string]*product)
 	inds := make([]int, 0, 3)
-	rxs := []*regexp.Regexp{regexp.MustCompile(`производитель|бренд`), regexp.MustCompile(`артикул`), regexp.MustCompile(`наименование|название`)}
+	rxs := []*regexp.Regexp{regexp.MustCompile(`производитель|бренд`), regexp.MustCompile(`артикул`), regexp.MustCompile(`наименование|название|имя`)}
+	curind := 1
+	var brandid int
 	for _, f := range files {
 		if f.IsDir() || (!strings.HasSuffix(f.Name(), ".csv")) {
 			continue
@@ -205,11 +259,11 @@ func (r *repo) loadproducts(path string) {
 		}
 	loop:
 		for i := 1; i < len(rows); i++ {
-			brand := strings.ToLower(rows[i][inds[0]])
+			brand_normname := strings.ToLower(rows[i][inds[0]])
 			art := rows[i][inds[1]]
 			name := rows[i][inds[2]]
 
-			if brand == "ngk" {
+			if brand_normname == "ngk" {
 				if ngkarts, ok := r.ngkArticules[art]; ok {
 					for k := 0; k < len(ngkarts); k++ {
 						if _, ok := r.productsByArticul[ngkarts[k]]; ok {
@@ -219,16 +273,48 @@ func (r *repo) loadproducts(path string) {
 					}
 				}
 			}
+			if b, ok := r.brandsByNorm[brand_normname]; ok {
+				brandid = b.id
+			} else {
+				println("brand not found by norm " + brand_normname)
+				continue loop
+			}
+
 			if _, ok := r.productsByArticul[art]; ok {
 				println("founded already added articul")
 				continue loop
+			} else {
+				r.productsByArticul[art] = &product{
+					id:       curind,
+					brand_id: brandid,
+					name:     name,
+					articul:  art,
+				}
+				curind++
 			}
 
 		}
 
 		inds = inds[:0]
 	}
+}
 
+func (r *repo) categorize() {
+	var c, max_subm, max_subm_id int
+	for _, prod := range r.productsByArticul {
+		for cat_id, cat := range r.categiriesById {
+			if c = countMaxMatchLength(prod.name, cat.phrases); c > max_subm {
+				max_subm = c
+				max_subm_id = cat_id
+			}
+		}
+		if max_subm_id > 0 {
+			prod.category_id = max_subm_id
+			max_subm_id, max_subm = 0, 0
+		} else {
+			println("did not found any category for product " + prod.name)
+		}
+	}
 }
 
 // ONLY " DELIMITERS AND ONLY , SEPARATORS
@@ -256,18 +342,60 @@ func (r *repo) loadNGKarticules(filepath string) {
 		if arts := rx.FindAllString(rows[i], -1); len(arts) < 2 {
 			continue
 		} else {
-			if _, ok := r.ngkArticules[arts[0]]; ok {
-				r.ngkArticules[arts[0]] = append(r.ngkArticules[arts[0]], arts[1])
-			} else {
-				r.ngkArticules[arts[0]] = []string{arts[1]}
-			}
-			if _, ok := r.ngkArticules[arts[1]]; ok {
-				r.ngkArticules[arts[1]] = append(r.ngkArticules[arts[1]], arts[0])
-			} else {
-				r.ngkArticules[arts[1]] = []string{arts[0]}
-			}
+			//if _, ok := r.ngkArticules[arts[0]]; ok {
+			r.ngkArticules[arts[0]] = append(r.ngkArticules[arts[0]], arts[1])
+			//} else {
+			//	r.ngkArticules[arts[0]] = []string{arts[1]}
+			//}
+			//if _, ok := r.ngkArticules[arts[1]]; ok {
+			r.ngkArticules[arts[1]] = append(r.ngkArticules[arts[1]], arts[0])
+			//} else {
+			//	r.ngkArticules[arts[1]] = []string{arts[0]}
+			//}
 		}
 	}
+}
+
+// substr must be lowered
+func countMaxMatchLength(str string, substr []string) (count int) {
+	var subcnt int
+	subrs := make([][]rune, len(substr))
+	for i := 0; i < len(substr); i++ {
+		subrs[i] = []rune(substr[i])
+	}
+	r := []rune(strings.ToLower(str))
+	for i, maxsubcnt := 0, 0; i < len(subrs); i, maxsubcnt = i+1, 0 {
+
+		//fmt.Println("\n$$$ word", string(subrs[i]))
+		//fmt.Println("--- subcnt-- =", 0, ",was", subcnt, ",maxsubcnt =", maxsubcnt)
+		subcnt = 0
+		for k, j := 0, 0; k < len(r) && j < len(subrs[i]); k++ {
+			//fmt.Println("+++ compare ", string(r[:k])+"["+string(r[k])+"]"+string(r[k+1:]), "with", string(subrs[i][:j])+"["+string(subrs[i][j])+"]"+string(subrs[i][j+1:]))
+			if r[k] != subrs[i][j] {
+				if subcnt > maxsubcnt {
+					//fmt.Println("=== maxsubcnt now", subcnt, ",was", maxsubcnt)
+					maxsubcnt = subcnt
+				}
+				subcnt = 0
+				j = 0
+			} else {
+				subcnt++
+				j++
+				//fmt.Println("--- subcnt++ =", subcnt)
+			}
+		}
+		if subcnt > maxsubcnt {
+			//fmt.Println("=+= maxsubcnt now", subcnt, ",was", maxsubcnt)
+			maxsubcnt = subcnt
+		}
+		//fmt.Println("--- maxsubcnt =", maxsubcnt)
+		if maxsubcnt > 2 {
+			//fmt.Println("### count now", count+maxsubcnt, ",was", count)
+			count += maxsubcnt
+		}
+	}
+	//fmt.Println("total count", count, subcnt, string(r), substr)
+	return
 }
 
 func unplural(str string) string {
